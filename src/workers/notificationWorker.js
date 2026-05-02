7139826333c56d1da6cf926fa545ea8716205876
Deepak Sharma <deepakmyside@gmail.com>
2026-05-02 23:08:36 +0530

    feat:  implement safe retry logic, idempotency handling for email processing in worker.

--- src/workers/notificationWorker.js
+++ src/workers/notificationWorker.js
@@ -8,22 +8,48 @@ const prisma = new PrismaClient()
 
 const worker = new Worker(
     "notifications", async (job) => {
+        console.log("Job ID:", job.id, "Attempt:", job.attemptsMade);
         const { notificationId} = job.data
       
         const notification = await prisma.notification.findUnique({
             where: { id: notificationId},
         });
 
+        // Idempotency check
+
+    if (notification.status === "sent"  || notification.status === "failed") {
+        console.log("Already sent, skipping", notificationId)
+        return;
+    }
+
+ try {
+
+    // success update 
+
     await sendEmail({
         to: notification.recipient,
         subject: "BMW M5 Purchase Bill🚗🚗⏩💲",
         message: notification.message
     });
-
+  
+    await prisma.notification.update({
+        where: {id: notificationId},
+        data: { status: "sent"},
+    });
     console.log("Email sent:", notificationId)
-    },
-    { connection },
 
-    console.log("worker runnning")
-)
+}catch(err) {
+    // failure update
+    await prisma.notification.update({
+        where: {id: notificationId},
+        data: {status:"failed"},
+    });
+
+    throw err;   /*this error is what it needs for bullmq to retry the queue and job logic*/
+}
+    },
+    {connection}  
+);
+console.log("Worker running....");
+
 
